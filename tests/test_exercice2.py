09d041efa66c57cf1c57f83d06e3b7930ef36ddc
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-18 18:40:51 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,4 +1,49 @@
 from exercices.exercice2 import *
+F = File()
 
-def test_tri_iteratif():
-    assert tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
+def test_F.est_vide():
+    assert F.est_vide() == True
+    
+F.enfile(2)
+F.affiche()
+
+def test_ F.est_vide():
+    assert  F.est_vide() == False
+    
+F.enfile(5)
+F.enfile(7)
+F.affiche()
+
+def test_F.defile():
+    assert F.defile() == 2
+    assert F.defile() == 5
+    
+def test_F.affiche():
+     assert F.affiche() == 7
+    
+    
+"""" 
+Voilà ce qui est dans le pdf
+>>> F = File()
+>>> F.est_vide()
+True
+>>> F.enfile(2)
+>>> F.affiche()
+2
+>>> F.est_vide()
+False
+>>> F.enfile(5)
+>>> F.enfile(7)
+>>> F.affiche()
+7
+5
+2
+>>> F.defile()
+2
+>>> F.defile()
+5
+>>> F.affiche()
+7
+"""
+
+    
