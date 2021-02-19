84b279fbe8de065405435ce5b935f378ed07d414
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-19 08:07:06 +0100

    Rectification des tests pour l'exo 2

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,49 +1,24 @@
 from exercices.exercice2 import *
 F = File()
 
-def test_F.est_vide():
+def test_F_est_vide():
     assert F.est_vide() == True
     
 F.enfile(2)
 F.affiche()
 
-def test_ F.est_vide():
+def test_ F_est_vide():
     assert  F.est_vide() == False
     
 F.enfile(5)
 F.enfile(7)
 F.affiche()
 
-def test_F.defile():
+def test_F_defile():
     assert F.defile() == 2
     assert F.defile() == 5
     
-def test_F.affiche():
+def test_F_affiche():
      assert F.affiche() == 7
     
-    
-"""" 
-Voilà ce qui est dans le pdf
->>> F = File()
->>> F.est_vide()
-True
->>> F.enfile(2)
->>> F.affiche()
-2
->>> F.est_vide()
-False
->>> F.enfile(5)
->>> F.enfile(7)
->>> F.affiche()
-7
-5
-2
->>> F.defile()
-2
->>> F.defile()
-5
->>> F.affiche()
-7
-"""
-
     
